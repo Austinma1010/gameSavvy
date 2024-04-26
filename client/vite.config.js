c4321d4fda6e0fb9eb495fee221f1509ef5ac9ba
moonflower <tayler.brianne.baldwin@gmail.com>
2024-04-25 23:50:20 -0400

    add pages, import app to main. writing app.jsx file, edited viteconfig, edited index.html

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    watch: {
-      usePolling: true,
-    },
-  },
+    port: 3000,
+    open: true,
+    proxy: {
+      '/graphql': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
