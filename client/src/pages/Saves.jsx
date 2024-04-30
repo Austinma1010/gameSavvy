676d62a6ad5f40231fe78bef361576a698dc6f21
jozsua smith <jozsua901@gmail.com>
2024-04-30 18:45:56 -0400

    made the save game page

---
+++ client/src/pages/Saves.jsx
@@ -0,0 +1,79 @@
+import { Button, Container, Card, SimpleGrid, } from '@chakra-ui/react'
+import { useQuery, useMutation } from '@apollo/client'
+import { QUERY_USER} from '../utils/queries';
+import auth from '../utils/auth';
+import { REMOVE_GAME } from '../utils/mutations';
+
+const saveGame = () => {
+    const {loading, data} = useQuery(QUERY_USER);
+    const [removeGame, {error}] = useMutation(REMOVE_GAME);
+
+    console.log("saveGame")
+    const userData = data?.user || {};
+    const savedGame = userData.trackedGame || [];
+
+    if (loading) {
+        return <h2>loading....</h2>
+    }
+
+    const deleteGame = async (gameid) => {
+        const token = auth.loggedIn() ? auth.getToken() : null;
+
+        if(!token) {
+            return false;
+        }
+
+        try{
+
+            const {data} = await removeGame({
+                variables: {gameid}
+            });
+
+            removeGame(gameid);
+        }
+        catch (err) {
+            console.error(err);
+        }
+    };
+
+
+    return (
+        <> 
+        <div flui1d>
+        <Container>
+        <h1>GameSavvy</h1>
+        <h2>{userData.username} saved games.</h2>
+        </Container>
+        </div>
+        <Container>
+            <h3>
+        {savedGame.length
+            ? `Viewing ${userData.saveGame.length} saved ${userData.saveGame.length === 1 ? 'game' : 'games'}:`
+            : 'You have no saved games!'}
+            </h3>
+            <SimpleGrid>
+                {userData.saveGame?.map((game) =>{
+                  return (
+                    <div key={game.gameid}>
+                        <Card>
+                            {game.image ? <Card.img src={game.image} alt={`the cover for ${game.title}`} /> : null}
+                            <Card.body>
+                                <Card.title>{game.title}</Card.title>
+                                <p>Sale price: {game.sale}</p>
+                                <p>retail price: {game.retail}</p>
+                                <Button onClick={() => deleteGame(game.gameid)}>
+                                    Remove game
+                                </Button>
+                            </Card.body>
+                        </Card>
+                    </div>
+                  )  
+                })}
+            </SimpleGrid>
+
+        </Container>
+        </>
+    );
+};
+
+export default saveGame;
