5e8c5d8185b068a133e78c177838203d90e75cc5
Austinma10 <austinma2168@gmail.com>
2024-05-04 19:09:30 -0400

    Minor fixes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,43 @@
-import React from 'react';import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Search';
 import Saves from './pages/Saves';
 import Login from './components/LoginForm';
 import Signup from './components/SignupForm';
 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <Home/>,
+      },
+      {
+        path: '/saves',
+        element: <Saves/>,
+      },
+      {
+        path: '/login',
+        element: <Login/>,
+      },
+      {
+        path: '/signup',
+        element: <Signup/>,
+      },
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
-);
+
+  <RouterProvider router={router} />
+)
 
 
 
